27bf73d473346a064613ae208ed4d175f648caa4
Nguyen Duc Hieu <94243225+2H1EU8@users.noreply.github.com>
2023-07-25 22:33:54 +0700

    Update books.js

--- src/api/books.js
+++ src/api/books.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const host = 'http://localhost:3021/';
+const host = 'https://my-json-server.typicode.com/2H1EU8/JS-MVCS/';
 
 export default axios.create({
   baseURL: host,
